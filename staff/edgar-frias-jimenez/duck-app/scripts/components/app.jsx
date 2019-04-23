2ea53c1fea771de07c33394c2b635665175a9816
Edgar Frías <edfriasji@gmail.com>
2019-04-23 20:05:46 +0200

    add register as a new component

---
+++ staff/edgar-frias-jimenez/duck-app/scripts/components/app.jsx
@@ -0,0 +1,65 @@
+const { Component, Fragment } = React
+
+class App extends Component {
+    state = { lang: 'en', visible: logic.isUserLoggedIn ? 'home' : 'landing', error: null, name: null }
+
+    handleLanguageChange = lang => this.setState({ lang })
+
+    handleRegisterNavigation = () => this.setState({ visible: 'register' })
+
+    handleLoginNavigation = () => this.setState({ visible: 'login' })
+
+    handleRegister = (name, surname, username, password) => {
+        logic.registerUser(name, surname, username, password, error => {
+            if (error) {
+                return this.setState({ error: error.message })
+            } else {
+                this.setState({ visible: 'login' })
+            }
+        })
+    }
+
+    handleLogin = (username, password) =>
+        logic.loginUser(username, password, error => {
+            if (error) return this.setState({ error: error.message })
+
+            logic.retrieveUser((error, user) => {
+                if (error) return this.setState({ error: error.message })
+
+                this.setState({ visible: 'home', name: user.name })
+            })
+        })
+
+    componentDidMount() {
+        logic.isUserLoggedIn && logic.retrieveUser((error, user) => {
+            if (error) return this.setState({ error: error.message })
+
+            this.setState({ name: user.name })
+        })
+    }
+
+    render() {
+        const {
+            state: { lang, visible, error, name },
+            handleLanguageChange,
+            handleRegisterNavigation,
+            handleLoginNavigation,
+            handleRegister,
+            handleLogin
+        } = this
+
+        // return <Fragment>
+        return <>
+            <LanguageSelector onLanguageChange={handleLanguageChange} />
+
+            {visible === 'landing' && <Landing lang={lang} onClickRegister={handleRegisterNavigation} onClickLogin={handleLoginNavigation} />}
+
+            {visible === 'register' && <Register lang={lang} onRegister={handleRegister} error={error} />}
+
+            {visible === 'login' && <Login lang={lang} onLogin={handleLogin} error={error} />}
+
+            {visible === 'home' && <Home lang={lang} name={name} />}
+        </>
+        // </Fragment>
+    }
+}
---
+++ staff/edgar-frias-jimenez/duck-app/scripts/components/register.jsx
@@ -0,0 +1,62 @@
+const i18nRegister = {
+  en: {
+      title: 'Register',
+      name: 'Name',
+      surname: 'Surname',
+      email: 'E-mail',
+      password: 'Password'
+  },
+  es: {
+      title: 'Registro',
+      name: 'Nombre',
+      surname: 'Apellido',
+      email: 'E-milio',
+      password: 'Contraseña'
+  },
+  ca: {
+      title: 'Registre',
+      name: 'Nom',
+      surname: 'Cognom',
+      email: 'E-mil·li',
+      password: 'Contrasenya'
+  },
+  ga: {
+      title: 'Rexistro',
+      name: 'Nome',
+      surname: 'Apelido',
+      email: 'E-miliño',
+      password: 'Contrasinal'
+  }
+};
+
+function Register(props) {
+  const { lang } = props;
+
+  const literals = i18nRegister[lang];
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const name = e.target.name.value
+    const surname = e.target.surname.value
+    const username = e.target.username.value
+    const password = e.target.password.value
+
+    props.onRegister(name, surname, username, password)
+  }
+
+  return <Fragment>
+    <h2>{literals.title}</h2>
+
+    <form onSubmit={handleSubmit}>
+      <ul>
+          <li><input type="text" name="name" placeholder={literals.name} /></li>
+          <li><input type="text" name="surname" placeholder={literals.surname} /></li>
+          <li><input type="text" name="username" placeholder={literals.email} /></li>
+          <li><input type="password" name="password" placeholder={literals.password} /></li>
+      </ul>
+
+      <button>{literals.title}</button>
+    </form>
+  </Fragment>
+}

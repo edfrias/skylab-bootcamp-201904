722f85e83c15fdc3018db425ec494e53f907de28
Edgar Frías <edfriasji@gmail.com>
2019-04-25 17:31:46 +0200

    update with Manu's version and erase unused folder

--- staff/edgar-frias-jimenez/duck-app/src/data/duck-api/index.js
+++ staff/edgar-frias-jimenez/duck-app/src/data/duck-api/index.js
@@ -1,26 +1,72 @@
 import validate from '../../common/validate'
 import call from '../../common/call'
 
-const duckApi = {
-    __url__: 'https://duckling-api.herokuapp.com/api',
+const userApi = {
+    __url__: 'https://skylabcoders.herokuapp.com/api',
+    __timeout__: 0,
 
-    searchDucks(query) {
+    create(username, password, data) {
         validate.arguments([
-            { name: 'query', value: query, type: 'string' }
+            { name: 'username', value: username, type: 'string', notEmpty: true },
+            { name: 'password', value: password, type: 'string', notEmpty: true },
+            { name: 'data', value: data, type: 'object', notEmpty: true, optional: true }
         ])
-        
-        return call(`${this.__url__}/search?q=${query}`)
+
+        return call(`${this.__url__}/user`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password, ...data }),
+            timeout: this.__timeout__
+        })
             .then(response => response.json())
     },
 
-    retrieveDuck(id) {
+    authenticate(username, password) {
         validate.arguments([
-            { name: 'id', value: id, type: 'string' }
+            { name: 'username', value: username, type: 'string', notEmpty: true },
+            { name: 'password', value: password, type: 'string', notEmpty: true }
         ])
 
-        return call(`${this.__url__}/ducks/${id}`)
+        return call(`${this.__url__}/auth`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+            timeout: this.__timeout__
+        })
             .then(response => response.json())
-    }
+    },
+
+    retrieve(id, token) {
+        validate.arguments([
+            { name: 'id', value: id, type: 'string', notEmpty: true },
+            { name: 'token', value: token, type: 'string', notEmpty: true }
+        ])
+
+        return call(`${this.__url__}/user/${id}`, {
+            headers: { Authorization: `Bearer ${token}` },
+            timeout: this.__timeout__
+        })
+            .then(response => response.json())
+    },
+
+    update(id, token, data) {
+        validate.arguments([
+            { name: 'id', value: id, type: 'string', notEmpty: true },
+            { name: 'token', value: token, type: 'string', notEmpty: true },
+            { name: 'data', value: data, type: 'object', notEmpty: true }
+        ])
+
+        return call(`${this.__url__}/user/${id}`, {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            timeout: this.__timeout__
+        })
+            .then(response => response.json())
+    },
 }
 
-export default duckApi
+export default userApi

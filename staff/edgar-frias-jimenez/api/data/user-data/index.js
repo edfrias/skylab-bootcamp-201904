cfea597b87017f57682db9f02a5ca2691c6e2e72
Edgar Frías <edfriasji@gmail.com>
2019-05-17 09:47:41 +0200

    add user data folder

---
+++ staff/edgar-frias-jimenez/api/data/user-data/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs').promises
+const path = require('path')
+const uuid = require('uuid/v4')
+const validate = require('../../common/validate')
+
+const userData = {
+    __file__: path.join(__dirname, 'users.json'),
+
+    create(user) {
+        validate.arguments([
+            { name: 'user', value: user, type: 'object', optional: false }
+        ])
+
+        user.id = uuid()
+
+        return fs.readFile(this.__file__, 'utf8')
+            .then(content => {
+                const users = JSON.parse(content)
+
+                users.push(user)
+
+                const json = JSON.stringify(users)
+
+                return fs.writeFile(this.__file__, json)
+            })
+    },
+
+    list() {
+        return fs.readFile(this.__file__, 'utf8')
+            .then(JSON.parse)
+    },
+
+    retrieve(userId) {
+        validate.arguments([
+            { name: 'userId', value: userId, type: 'string', notEmpty: true, optional: false }
+        ])
+
+        return fs.readFile(this.__file__, 'utf8')
+            .then(content => {
+                const users = JSON.parse(content)
+                return users.find(item => item.id === userId)
+            })
+    },
+}
+
+module.exports = userData
